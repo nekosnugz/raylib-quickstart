0229f406f5f0e980aafd794b223443f65a88cc5b
issVigano-rossio08 <rossi.orlando@issvigano.org>
2025-02-13 22:18:26 +0100

    Start

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "raylib.h"
+
+int main()
+{
+    const int windowWidth = 800;
+    const int windowHeight = 600;
+
+    InitWindow(windowWidth, windowHeight, "RAYLIB TEST - DO NOT PANIC");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+----------------
+    return 0;
+}
